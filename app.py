bc6f3422b0d21c73ea3fa61b90de9ef540cd2aa1
Lee Vaughn <leev@Lees-MacBook-Pro.local>
2021-11-14 10:29:26 -0800

    Add functionality to send and receive data

--- app.py
+++ app.py
@@ -21,46 +21,50 @@ stream = p.open(
     frames_per_buffer = FRAMES_PER_BUFFER
 )
 
+#
 async def send_receive():
     print(f"Connecting websocket to url ${URL}")
     async with websockets.connect(
         URL,
-        extra_headers = (("Authorization", auth_key)),
-        ping_interval = 5,
-        ping_timeout = 20
+        extra_headers=(("Authorization", auth_key),),
+        ping_interval=5,
+        ping_timeout=20
     ) as _ws:
         await asyncio.sleep(0.1)
-        print("Receiving SessionBegins...")
+        print("Receiving SessionBegins ...")
         session_begins = await _ws.recv()
         print(session_begins)
-        print("Sending messages...")
+        print("Sending messages ...")
         async def send():
             while True:
                 try:
                     data = stream.read(FRAMES_PER_BUFFER)
-                    data = base64.b64decode(data).decode("utf-8")
-                    json_data = json.dumps({"audio_data": str(data)})
+                    data = base64.b64encode(data).decode("utf-8")
+                    json_data = json.dumps({"audio_data":str(data)})
                     await _ws.send(json_data)
-                except websockets.exceptions.ConnectClosedError as e:
+                except websockets.exceptions.ConnectionClosedError as e:
                     print(e)
                     assert e.code == 4008
                     break
                 except Exception as e:
                     assert False, "Not a websocket 4008 error"
                 await asyncio.sleep(0.01)
-
+            
             return True
-
+        
         async def receive():
             while True:
                 try:
                     result_str = await _ws.recv()
                     print(json.loads(result_str)["text"])
-                except websockets.exceptions.ConnectClosedError as e:
+                except websockets.exceptions.ConnectionClosedError as e:
                     print(e)
                     assert e.code == 4008
                     break
                 except Exception as e:
                     assert False, "Not a websocket 4008 error"
-                
+        
         send_result, receive_result = await asyncio.gather(send(), receive())
+
+
+asyncio.run(send_receive())
