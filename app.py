6b9594a1a66fd31b62c7455970f5b9ff462dde13
Lee Vaughn <leev@Lees-MacBook-Pro.local>
2021-11-14 09:30:51 -0800

    Add function to start recording

--- app.py
+++ app.py
@@ -6,3 +6,12 @@ FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 16000
 p = pyaudio.PyAudio()
+
+# starts recording
+stream = p.open(
+    format = FORMAT
+    channels = CHANNELS
+    rate = RATE
+    input = True
+    frames_per_buffer = FRAMES_PER_BUFFER
+)
